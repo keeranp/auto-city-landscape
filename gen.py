4986d1472d1e6bfab3d702b46c496c8ca801899c
keeranp <keeran.paheerathan@gmail.com>
2021-12-12 09:29:43 +0100

    Added shading depending on the depth of the building

--- gen.py
+++ gen.py
@@ -10,7 +10,7 @@ def random_rectangle(img, depth):
         int(depth * img_size[0]/4), img_size[0]))
     pt2 = (max(pt1[0]+100, randrange(pt1[0], pt1[0] + 200)), img_size[0])
 
-    color = night_filter((randrange(255), randrange(255), randrange(255)))
+    color = night_filter((randrange(255), randrange(255), randrange(255)), depth)
 
     cv2.rectangle(img, pt1, pt2, color, -1)
 
@@ -29,12 +29,12 @@ def random_moon(img):
     cv2.circle(img, pt1, 120, (250, 250, 250), -1)
 
 
-def night_filter(color):
+def night_filter(color, depth):
     B = 1
     G = .5
     R = .5
 
-    return (color[0] * B, color[1] * G, color[2] * R)
+    return (color[0] * B/(depth * depth), color[1] * G/(depth * depth), color[2] * R/(depth * depth))
 
 
 def sky(img):
@@ -48,7 +48,7 @@ def sky(img):
         total += step
 
 
-def crop(img,w,h):
+def crop(img, w, h):
     center = (img.shape[0]/2, img.shape[1]/2)
     x = center[1] - w/2
     y = center[0] - h/2
@@ -66,13 +66,17 @@ for i in range(1000):
 random_moon(img)
 
 for i in range(100):
-    random_rectangle(img, 1.5)
-    random_rectangle(img, 2.3)
+    random_rectangle(img, 1.25)
+
+for i in range(150):
+    random_rectangle(img, 2)
+
+for i in range(200):
     random_rectangle(img, 3)
 
-img = crop(img,1280,720)
+img = crop(img, 1280, 720)
 
 cv2.imshow('test', img)
 cv2.waitKey()
 
-cv2.imwrite('test.png',img)
+cv2.imwrite('test.png', img)
